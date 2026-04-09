45d4dc07f6d727c766449220a33796e84acbe470
zjjlivein <1834792141@qq.com>
2026-04-09 16:45:51 +0800

    fix huggingface_hub download

--- scripts/regression/ci_model_unittest.sh
+++ scripts/regression/ci_model_unittest.sh
@@ -29,6 +29,7 @@ install_requirements() {
     python -m pip config --user set global.trusted-host pypi.tuna.tsinghua.edu.cn
     python -m pip uninstall paddlepaddle paddlepaddle_gpu paddlefleet -y
     # python -m pip install -U --no-cache-dir transformers 
+    python -m pip install --upgrade pip setuptools wheel
     python -m pip install -r requirements.txt -i https://pypi.org/simple
     # echo "paddlepaddle-gpu @ https://paddle-qa.bj.bcebos.com/paddle-pipeline/Release-TagBuild-Training-Linux-Gpu-Cuda12.9-Cudnn9.9-Trt10.5-Mkl-Avx-Gcc11-SelfBuiltPypiUse/cbf3469113cd76b7d5f4cba7b8d7d5f55d9e9911/paddlepaddle_gpu-3.3.0-cp310-cp310-linux_x86_64.whl" >> requirements.txt
     python setup.py bdist_wheel > /dev/null
